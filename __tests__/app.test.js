699cf125f09a98bb810662850664ba8aac0280d0
MoikeCheck <mikewheatley@hotmail.co.uk>
2022-02-16 20:12:29 +0000

    GET /api/users ticket completed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -67,7 +67,19 @@ describe("GET /api/articles/:article_id", () => {
   });
 });
 
-
 describe("GET /api/users", () => {
-  
+  test("should respond with an array of user objects", () => {
+    return request(app)
+      .get("/api/users")
+      .expect(200)
+      .then(({ body }) => {
+        body.users.forEach((element) => {
+          expect(element).toMatchObject({
+            username: expect.any(String),
+            name: expect.any(String),
+            avatar_url: expect.any(String),
+          });
+        });
+      });
+  });
 });
