e4825ccfd896192592b769911eec462f8e883e37
MoikeCheck <mikewheatley@hotmail.co.uk>
2022-02-17 11:07:00 +0000

    test added to GET /apo/users to test array length

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -72,13 +72,14 @@ describe("GET /api/users", () => {
     return request(app)
       .get("/api/users")
       .expect(200)
-      .then(({ body }) => {
-        body.users.forEach((element) => {
+      .then(({ body: { users } }) => {
+        users.forEach((element) => {
           expect(element).toMatchObject({
             username: expect.any(String),
             name: expect.any(String),
             avatar_url: expect.any(String),
           });
+          expect(users.length).toBe(4);
         });
       });
   });
