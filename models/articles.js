d19ae7da5af9c5d129b12705577d1688873b5c47
MoikeCheck <mikewheatley@hotmail.co.uk>
2022-02-17 11:04:01 +0000

    comment removed

--- models/articles.js
+++ models/articles.js
@@ -7,18 +7,15 @@ exports.selectArticles = () => {
 };
 
 exports.selectArticlesById = (articleID) => {
-  return (
-    db
-      //$1 is preventing SQL injection
-      .query("SELECT * FROM articles WHERE article_id = $1;", [articleID])
-      .then(({ rows }) => {
-        if (rows.length === 0) {
-          return Promise.reject({
-            status: 404,
-            msg: `No article found by this ID`,
-          });
-        }
-        return rows[0];
-      })
-  );
+  return db
+    .query("SELECT * FROM articles WHERE article_id = $1;", [articleID])
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: `No article found by this ID`,
+        });
+      }
+      return rows[0];
+    });
 };
